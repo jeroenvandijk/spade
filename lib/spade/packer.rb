4e33f7ed3f1ffcc0bfe0df0fe778612defc3ddb9
Nick Quaranto <nick@quaran.to>
2011-02-02 17:37:24 -0500

    start shoving metadata into the requirements field of the gemspec

--- lib/spade/packer.rb
+++ lib/spade/packer.rb
@@ -1,15 +1,25 @@
 module Spade
   class Packer
-    def initialize(path)
-      @path = path
+    def initialize(path, email)
+      @path  = path
+      @email = email
     end
 
+    METADATA = %w[keywords licenses engines main bin]
+
     def transform
-      package = JSON.parse(File.read(@path))
+      package  = JSON.parse(File.read(@path))
+      metadata = Hash[*package.select { |k, v| METADATA.include?(k) }.flatten(1)]
+
       Gem::Specification.new do |spec|
-        spec.name    = package["name"]
-        spec.version = package["version"]
-        spec.authors = [package["author"]]
+        spec.name         = package["name"]
+        spec.version      = package["version"]
+        spec.authors      = [package["author"]]
+        spec.email        = @email
+        spec.homepage     = package["homepage"]
+        spec.summary      = package["description"]
+        spec.description  = package["description"]
+        spec.requirements = [metadata.to_json]
       end
     end
   end
--- spec/packer_spec.rb
+++ spec/packer_spec.rb
@@ -1,16 +1,46 @@
 require "spec_helper"
 
-describe Spade::Packer do
+describe Spade::Packer, "transforming" do
+  let(:email) { "user@example.com" }
+
   subject do
-    Spade::Packer.new(fixtures("package.json"))
+    packer = Spade::Packer.new(fixtures("package.json"), email)
+    packer.transform
   end
 
-  it "transforms a package.json into a gemspec" do
-    gemspec = subject.transform
-    gemspec.class.should == Gem::Specification
+  it "transforms the name" do
+    subject.name.should == "coffee-script"
+  end
 
-    gemspec.name.should     == "coffee-script"
-    gemspec.version.should  == Gem::Version.new("1.0.1.pre")
-    gemspec.authors.should  == ["Jeremy Ashkenas"]
+  it "transforms the version" do
+    subject.version.should == Gem::Version.new("1.0.1.pre")
+  end
+
+  it "transforms the author" do
+    subject.authors.should == ["Jeremy Ashkenas"]
+  end
+
+  it "transforms the email" do
+    subject.email.should == email
+  end
+
+  it "transforms the homepage" do
+    subject.homepage.should == "http://github.com/jashkenas/coffee-script"
+  end
+
+  it "transforms the description" do
+    subject.description.should == "Unfancy JavaScript"
+  end
+
+  it "packs metadata into requirements" do
+    metadata = JSON.parse(subject.requirements.first)
+    metadata["keywords"].should == %w[javascript language coffeescript compiler]
+    metadata["licenses"].should == [
+      {"type" => "MIT",
+       "url"  => "http://github.com/jashkenas/coffee-script/raw/master/LICENSE"}
+    ]
+    metadata["engines"].should == {"node" => ">=0.2.5"}
+    metadata["main"].should == "./lib/coffee-script"
+    metadata["bin"].should == {"coffee" => "./bin/coffee", "cake" => "./bin/cake"}
   end
 end
