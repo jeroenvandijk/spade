b5e01780d96475ff653bfcd2614c059cd8da6aee
Nick Quaranto <nick@quaran.to>
2011-02-03 11:44:02 -0500

    Starting to expand files out in the gemspec

--- lib/spade/packer.rb
+++ lib/spade/packer.rb
@@ -8,9 +8,6 @@ module Spade
     METADATA = %w[keywords licenses engines main bin]
 
     def transform
-      package  = JSON.parse(File.read(@path))
-      metadata = Hash[*package.select { |k, v| METADATA.include?(k) }.flatten(1)]
-
       Gem::Specification.new do |spec|
         spec.name         = package["name"]
         spec.version      = package["version"]
@@ -20,7 +17,35 @@ module Spade
         spec.summary      = package["description"]
         spec.description  = package["description"]
         spec.requirements = [metadata.to_json]
+        spec.files        = glob_javascript(lib_path) + bin_files
+        spec.test_files   = glob_javascript(test_path)
       end
+    end
+
+    private
+
+    def glob_javascript(path)
+      Dir[File.join(path, "**", "*.js")]
+    end
+
+    def bin_files
+      package["bin"].values
+    end
+
+    def lib_path
+      package["directories"]["lib"]
+    end
+
+    def test_path
+      package["directories"]["test"]
+    end
+
+    def metadata
+      Hash[*package.select { |k, v| METADATA.include?(k) }.flatten(1)]
+    end
+
+    def package
+      @package ||= JSON.parse(File.read(@path))
     end
   end
 end
--- spec/packer_spec.rb
+++ spec/packer_spec.rb
@@ -3,6 +3,10 @@ require "spec_helper"
 describe Spade::Packer, "transforming" do
   let(:email) { "user@example.com" }
 
+  before do
+    cd(home)
+  end
+
   subject do
     packer = Spade::Packer.new(fixtures("package.json"), email)
     packer.transform
@@ -42,5 +46,21 @@ describe Spade::Packer, "transforming" do
     metadata["engines"].should == {"node" => ">=0.2.5"}
     metadata["main"].should == "./lib/coffee-script"
     metadata["bin"].should == {"coffee" => "./bin/coffee", "cake" => "./bin/cake"}
+  end
+
+  def expand_sort(files)
+    files.sort.map { |f| File.expand_path(f) }
+  end
+
+  it "expands paths from the directories" do
+    others     = ["lib/coffee.png", "qunit/test.log"]
+    files      = ["bin/coffee", "bin/cake", "lib/coffee.js", "lib/coffee/base.js", "lib/coffee/mocha/chai.js"]
+    test_files = ["qunit/test.js", "qunit/coffee/test.js"]
+
+    FileUtils.mkdir_p(["bin/", "lib/coffee/", "lib/coffee/mocha", "qunit/", "qunit/coffee"])
+    FileUtils.touch(files + test_files + others)
+
+    expand_sort(subject.files).should == expand_sort(files + test_files)
+    expand_sort(subject.test_files).should == expand_sort(test_files)
   end
 end
