959fe35fddc87a5902aaa4876c11023038b038a5
Nick Quaranto <nick@quaran.to>
2011-02-10 14:48:13 -0500

    gauntlet up jquery

--- spec/gauntlet_spec.rb
+++ spec/gauntlet_spec.rb
@@ -9,6 +9,7 @@ describe "spade build the gauntlet" do
 
   {
     "ivory"    => "0.0.1",
+    "jquery"   => "1.4.3",
     "optparse" => "1.0.1",
   }.each do |package, version|
     it "builds a spade from #{package}" do
