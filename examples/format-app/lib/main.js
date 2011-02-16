d5b77c91853f332dfa1aa4d232d1a7901540fd37
Peter Wagenet <peter.wagenet@gmail.com>
2011-02-15 16:29:30 -0800

    Updated format-app example for running from the CLI

--- examples/format-app/lib/main.js
+++ examples/format-app/lib/main.js
@@ -1,4 +1,5 @@
-
+// Allows us to run from the command line
+if (typeof exports === 'undefined') var exports = {};
 exports.README = require('format-app/~resources/README');
 exports.data   = require('format-app/~resources/config');
 
