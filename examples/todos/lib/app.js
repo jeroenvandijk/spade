9c55c66e91638a06dd0534b35eaa469b8f06ae3a
Peter Wagenet <peter.wagenet@gmail.com>
2011-02-25 13:12:09 -0800

    Todos Example

---
+++ examples/todos/lib/app.js
@@ -0,0 +1,78 @@
+// Create the application namespace.
+Todos = SC.Application.create();
+
+//
+// MODELS
+//
+
+Todos.Todo = SC.Object.extend({
+  init: function() {
+    if (!this.get('title')) { this.set('title', "empty todo..."); }
+
+    return arguments.callee.base.apply(this, arguments);
+  },
+
+  title: null,
+  isDone: false
+});
+
+//
+// CONTROLLERS
+//
+
+Todos.todoListController = SC.ArrayController.create({
+  content: [],
+
+  // Creates a new todo with the passed title, then adds it
+  // to the array.
+  createTodo: function(title) {
+    var todo = Todos.Todo.create({ title: title });
+    this.pushObject(todo);
+  }
+});
+
+//
+// VIEWS
+//
+
+Todos.CreateTodoView = SC.TemplateView.create(SC.TextFieldSupport, {
+
+  insertNewline: function() {
+    Todos.todoListController.createTodo(this.get('value'));
+
+    this.set('value', '');
+    return true;
+  }
+});
+
+Todos.todoListView = SC.TemplateCollectionView.create({
+  contentBinding: 'Todos.todoListController',
+
+  itemView: SC.TemplateView.extend({
+    // Add the 'done' class to this view
+    // if the Todo object is marked isDone
+    isDoneDidChange: function() {
+      var isDone = this.getPath('content.isDone');
+
+      this.$().toggleClass('done', isDone);
+    }.observes('.content.isDone')
+  })
+});
+
+Todos.CheckboxView = SC.TemplateView.extend(SC.CheckboxSupport, {
+
+  // Observe the value property
+  valueDidChange: function() {
+    // Get the Todo object and change its isDone property
+    this.setPath('parentView.content.isDone', this.get('value'));
+  }.observes('value')
+});
+
+jQuery(document).ready(function() {
+  $(document.body).html(''); // Clear loading message
+  Todos.mainPane = SC.TemplatePane.append({
+    layerId: "todoapp",
+    templateName: "app"
+  });
+});
+
---
+++ examples/todos/lib/main.js
@@ -0,0 +1,19 @@
+require('sproutcore-corefoundation');
+
+require('./app');
+
+var appTemplate =
+  "<h1>Todos</h1>\n" +
+  "{{#view \"Todos.CreateTodoView\"}}\n" +
+  "<input id=\"new-todo\" type=\"text\"\n" +
+  "       placeholder=\"What needs to be done?\" />\n" +
+  "{{/view}}\n" +
+  "\n" +
+  "{{#collection \"Todos.todoListView\"}}\n" +
+  "  {{#view \"Todos.CheckboxView\"}}\n" +
+  "    <input class=\"check\" type=\"checkbox\" />\n" +
+  "  {{/view}}\n" +
+  "  <div class='todo-content'>{{title}}</div>\n" +
+  "{{/collection}}";
+
+SC.TEMPLATES.set("app", SC.Handlebars.compile(appTemplate));
