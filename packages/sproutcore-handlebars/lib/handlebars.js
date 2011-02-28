3c5a5b60d6610bc34131aa2612c6e24777a5b9cb
Peter Wagenet <peter.wagenet@gmail.com>
2011-02-28 10:54:26 -0800

    Added Handlebars Format for Spade

---
+++ packages/sproutcore-handlebars/lib/handlebars.js
@@ -0,0 +1,5 @@
+exports.compileFormat = function(code, spade, filename){
+  var name = filename.replace(/^[^\/]+\//, '').replace(/\.handlebars$/, '');
+  var escaped = code.replace(/"/g,'\\"').replace(/\n/g, '\\n');
+  return "SC.TEMPLATES[\""+name+"\"] = SC.Handlebars.compile(\""+escaped+"\")";
+};
