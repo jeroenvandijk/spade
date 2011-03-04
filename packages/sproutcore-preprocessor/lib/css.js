0112d743e4f87f685480c7f354eb295233849db0
Peter Wagenet <peter.wagenet@gmail.com>
2011-03-03 22:45:43 -0800

    Basic support for sc_static

--- packages/sproutcore-preprocessor/lib/css.js
+++ packages/sproutcore-preprocessor/lib/css.js
@@ -1,11 +1,15 @@
-exports.compileFormat = function(code){
-  var escaped = code.replace(/"/g,'\\"').replace(/\n/g, '\\n');
+exports.compileFormat = function(code, sandbox, filename, format, pkg){
+  // Handle sc_static|static_url
+  code = require('./sc_static').replace(code, sandbox, pkg, "url('{url}')");
+
+  // escape
+  code = code.replace(/"/g,'\\"').replace(/\n/g, '\\n');
 
   var ret =
     "if (window.document && window.document.head) {\n"+
     "  var el = document.createElement('style');\n"+
     "  el.type = \"text/css\";\n"+
-    "  el.innerHTML = \""+escaped+"\";\n"+
+    "  el.innerHTML = \""+code+"\";\n"+
     "  document.head.appendChild(el);\n"+
     "} else {\n"+
     "  console.error(\"Can't handle css outside of browser!\");\n"+
--- packages/sproutcore-preprocessor/lib/preprocessor.js
+++ packages/sproutcore-preprocessor/lib/preprocessor.js
@@ -1,5 +1,10 @@
-// Handle sc_super()
-exports.compilePreprocessor = function(code){
- return code.replace(/sc_super\(\s*\)/g, 'arguments.callee.base.apply(this, arguments)');
+exports.compilePreprocessor = function(code, sandbox, filename, pkg){
+  // Handle sc_super()
+  code = code.replace(/sc_super\(\s*\)/g, 'arguments.callee.base.apply(this, arguments)');
+
+  // Handle sc_static|static_url
+  code = require('./sc_static').replace(code, sandbox, pkg);
+
+  return code;
 };
 
