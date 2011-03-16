e52106027df4da4b26fa12d7ac9d9365d058412a
Peter Wagenet <peter.wagenet@gmail.com>
2011-03-16 14:44:09 -0700

    Finished Sandbox misc tests, added better error message

--- spec/javascript/sandbox/misc.js
+++ spec/javascript/sandbox/misc.js
@@ -11,16 +11,48 @@ var Ct = require('core-test/sync'),
 
 Ct.module('spade: Sandbox Miscellaneous');
 
-Ct.test('toString', function(t){
-  var sandbox = new Sandbox(new Spade(), 'Test Sandbox');
-
-  t.equal(sandbox.toString(), '[Sandbox Test Sandbox]');
+Ct.setup(function(t){
+  t.sandbox = new Sandbox(new Spade(), 'Test Sandbox');
 });
 
-Ct.test("exists", pending);
+Ct.teardown(function(t){
+  delete t.sandbox;
+});
 
-Ct.test("async", pending);
+Ct.test('toString', function(t){
+  t.equal(t.sandbox.toString(), '[Sandbox Test Sandbox]');
+});
 
-Ct.test("url", pending);
+Ct.test("exists", function(t){
+  t.sandbox.spade.register('test', { name: 'test' });
+  t.sandbox.spade.register('test/main', '');
 
-Ct.test("destroy", pending);
+  t.ok(t.sandbox.exists('test'), "test should exist");
+  t.ok(!t.sandbox.exists('missing'), "missing should not exist");
+});
+
+Ct.test("async", function(t){
+  t.sandbox.spade.register('test', { name: 'test' });
+  t.sandbox.spade.register('test/main', 'exports.hello = "hi";');
+
+  t.deepEqual(t.sandbox.async('test'), {
+    "data": "exports.hello = \"hi\";",
+    "filename": "test/main",
+    "format": "js"
+  });
+});
+
+Ct.test("url", function(t){
+  t.sandbox.spade.register('no-root', { name: 'no-root' });
+  t.sandbox.spade.register('with-root', { name: 'with-root', root: 'root/url' });
+
+  t.throws(function(){ t.sandbox.url('missing') }, "Can't get url for non-existent package missing/main");
+  t.throws(function(){ t.sandbox.url('no-root') }, "Package for no-root/main does not support urls");
+  t.equal(t.sandbox.url('with-root'), 'root/url/main');
+});
+
+Ct.test("destroy", function(t){
+  t.equal(t.sandbox.isDestroyed, false);
+  t.sandbox.destroy();
+  t.equal(t.sandbox.isDestroyed, true);
+});
