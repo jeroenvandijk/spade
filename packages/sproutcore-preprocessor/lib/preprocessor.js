2f8fe484548dedfdd297fce7d4197e71e5e02b83
Peter Wagenet <peter.wagenet@gmail.com>
2011-03-15 18:59:35 -0700

    Fixed circular preprocessor issue

--- packages/sproutcore-preprocessor/lib/preprocessor.js
+++ packages/sproutcore-preprocessor/lib/preprocessor.js
@@ -1,9 +1,11 @@
+var scStatic = require('./sc_static');
+
 exports.compilePreprocessor = function(code, sandbox, filename, pkg){
   // Handle sc_super()
   code = code.replace(/sc_super\(\s*\)/g, 'arguments.callee.base.apply(this, arguments)');
 
   // Handle sc_static|static_url
-  code = require('./sc_static').replace(code, sandbox, pkg);
+  code = scStatic.replace(code, sandbox, pkg);
 
   return code;
 };
