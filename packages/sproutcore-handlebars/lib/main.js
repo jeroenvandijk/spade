0521c57679df0a158e7fe5a41b6cf75c8c10dcfb
Peter Wagenet <peter.wagenet@gmail.com>
2011-03-15 19:48:32 -0700

    Updated SproutCore

--- packages/sproutcore-handlebars/lib/main.js
+++ packages/sproutcore-handlebars/lib/main.js
@@ -7,5 +7,9 @@ window.SC = window.SC || {};
 window.Handlebars = require('sproutcore-handlebars/~framework/handlebars');
 
 sc_require('extensions');
+sc_require('extensions/bind');
+sc_require('extensions/collection');
+sc_require('extensions/localization');
+sc_require('extensions/view');
 
 return SC;
