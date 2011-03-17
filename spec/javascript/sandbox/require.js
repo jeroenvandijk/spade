aa8bed00ed04dcf2ec3c63a1d4fbe760d551f3f5
Peter Wagenet <peter.wagenet@gmail.com>
2011-03-17 14:42:43 -0700

    Sandbox require tests

--- spec/javascript/sandbox/require.js
+++ spec/javascript/sandbox/require.js
@@ -14,6 +14,8 @@ Ct.module('spade: Sandbox require');
 
 Ct.setup(function(t) {
   t.sandbox = new Sandbox(new Spade()); 
+  t.sandbox.spade.register('testing', { name: 'testing' });
+  t.sandbox.spade.register('testing/main', "exports.hello = 'hi';");
 });
 
 Ct.teardown(function(t) { 
@@ -21,14 +23,26 @@ Ct.teardown(function(t) {
 });
 
 Ct.test("require new", function(t){
-  t.sandbox.spade.register('testing', { name: 'testing' });
-  t.sandbox.spade.register('testing/main', "exports.hello = 'hi';");
   t.equal(t.sandbox.require('testing').hello, 'hi');
 });
 
-Ct.test("require existing", pending);
+// NOTE: This test doesn't necessarily tell us that anything special is happening, just that it works
+Ct.test("require existing", function(t){
+  // Cache it
+  t.sandbox.require('testing');
+  // Now require again
+  t.equal(t.sandbox.require('testing').hello, 'hi');
+});
 
+// TODO: I'm not actually sure how this should work - PW
 Ct.test("require circular", pending);
+/*
+Ct.test("require circular", function(t){
+  t.sandbox.spade.register('testing/file1', "exports.value = require('testing/file2').value * 2;");
+  t.sandbox.spade.register('testing/file2', "exports.value = require('testing/file1').value * 2;");
+  t.equal(t.sandbox.require('testing/file1').value, 4);
+});
+*/
 
 Ct.test("throw if doesn't exist", function(t){
   t.throws(function(){ t.sandbox.require('missing'); }, "Module missing not found");
