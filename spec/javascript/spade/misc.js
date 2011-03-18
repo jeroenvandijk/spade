e5abb3965a91bf38b319129f326c222da6ef41a3
Peter Wagenet <peter.wagenet@gmail.com>
2011-03-17 17:33:00 -0700

    Added more placeholder tests

---
+++ spec/javascript/spade/misc.js
@@ -0,0 +1,23 @@
+// ==========================================================================
+// Project:   Spade - CommonJS Runtime
+// Copyright: ©2011 Strobe Inc. All rights reserved.
+// License:   Licened under MIT license (see __preamble__.js)
+// ==========================================================================
+//
+
+var Ct = require('core-test/sync');
+
+Ct.module('spade: Spade miscellaneous');
+
+Ct.test('globalize works', function(t){ console.log("PENDING: "+t.toString()); });
+
+Ct.test('noConflict works', function(t){ console.log("PENDING: "+t.toString()); });
+
+Ct.test('sandbox works', function(t){ console.log("PENDING: "+t.toString()); });
+
+Ct.test('factoryExists works', function(t){ console.log("PENDING: "+t.toString()); });
+
+Ct.test('package works', function(t){ console.log("PENDING: "+t.toString()); });
+
+
+
