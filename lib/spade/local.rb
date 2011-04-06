9341cfd13ca1f5a837f4954e5aae47ccfedda36d
Peter Wagenet <peter.wagenet@gmail.com>
2011-04-06 10:20:39 -0700

    Fixed `spade uninstall` warning

--- lib/spade/local.rb
+++ lib/spade/local.rb
@@ -1,17 +1,10 @@
 module Spade
   class Local < Repository
     def uninstall(package)
-      index = Gem::SourceIndex.from_gems_in(env.spade_dir("specifications"))
-      index.refresh!
-      specs = index.find_name package
-
-      if specs.first
-        uninstaller = Gem::Uninstaller.new(package, :ignore => true)
-        uninstaller.uninstall_gem(specs.first, specs)
-        true
-      else
-        false
-      end
+      Gem::Uninstaller.new(package, :ignore => true).uninstall
+      true
+    rescue Gem::InstallError
+      false
     end
 
     def pack(path)
