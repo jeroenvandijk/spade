514d9f9206a30cf24a66de65bb6ed15451c0d565
Peter Wagenet <peter.wagenet@gmail.com>
2011-04-18 09:57:53 -0700

    Whitespace cleanup

--- spec/javascript/require-test.js
+++ spec/javascript/require-test.js
@@ -9,37 +9,37 @@ var Ct = require('core-test/sync'),
 
 // ..........................................................
 // BASIC REQUIRE
-// 
+//
 
 Ct.module('spade: basic require');
 
 Ct.setup(function(t) {
-  t.spade = new Spade(); 
+  t.spade = new Spade();
 });
 
-Ct.teardown(function(t) { 
+Ct.teardown(function(t) {
   delete t.spade;
 });
 
 
 Ct.test('register then require a module', function(t) {
   var spade = t.spade;
-  
-  spade.register('foo/bar', function(require, exports) { 
+
+  spade.register('foo/bar', function(require, exports) {
     exports.foo = 'bar';
   });
-  
+
   var exp = spade.require('foo/bar');
-  t.equal(exp.foo, 'bar', 'exports.foo == bar - means require succeeded');  
+  t.equal(exp.foo, 'bar', 'exports.foo == bar - means require succeeded');
 });
 
 Ct.test('register a string factory then require', function(t) {
   var spade = t.spade;
-  
+
   spade.register('foo/bar', "exports.foo = 'bar';");
-  
+
   var exp = spade.require('foo/bar');
-  t.equal(exp.foo, 'bar', 'exports.foo == bar - means require succeeded');  
+  t.equal(exp.foo, 'bar', 'exports.foo == bar - means require succeeded');
 });
 
 Ct.test('require a non-existant module will throw an exception', function(t) {
